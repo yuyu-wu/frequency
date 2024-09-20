b2efc61e9bf65fc4f22194d48147872311658ae2
yuyu <swharriet2012@gmail.com>
2024-09-20 14:46:23 +1000

    initial

---
+++ frequency.py
@@ -0,0 +1,5 @@
+# frequency
+# Write a function called frequency. This function accepts a list and a search_term (this will always be a primitive value) and returns the number of times the search_term appears in the list.
+
+def frequency(collection, searchTerm):
+    return collection.count(searchTerm)
